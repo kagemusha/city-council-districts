cc680662e726962b87a85eae506aad3a425eee44
kagemusha <mmadrid99@gmail.com>
2019-05-03 17:55:09 -0400

    flask app

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from city_council_lookup import CityCouncilDistrictLookup
+from urllib.parse import unquote
+
+app = Flask(__name__)
+district_lookup = CityCouncilDistrictLookup()
+@app.route('/')
+def addr_to_city_council_member():
+  addr = request.args.get('addr')
+  if not addr:
+    return "Need an address"
+  addr = unquote(addr)
+  return str(district_lookup.addr_to_district(addr))
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
